05940f92bfea92d089af3a6e642ddc0af9c62263
Matthew Burns <burns.97@gmail.com>
2026-05-23 11:24:46 -0400

    Expand bundled airport seed

--- tests/test_airports.py
+++ tests/test_airports.py
@@ -1,6 +1,17 @@
 from post_trip_summary.geo.airports import resolve_airport_candidate
 
 
+def test_bundled_airport_seed_has_broad_travel_coverage():
+    from post_trip_summary.geo.airports import load_airports
+
+    airports = load_airports()
+    iata_codes = {airport.iata_code for airport in airports}
+
+    assert len(airports) >= 1000
+    for code in ("AKL", "CMH", "CUN", "DFW", "HNL", "LHR", "NRT"):
+        assert code in iata_codes
+
+
 def test_resolve_airport_candidate_finds_cmh_for_airport_context():
     candidate = resolve_airport_candidate(
         39.99786388888889,
